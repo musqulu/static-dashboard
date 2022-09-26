5067df99cb222030d1b08b6a6b1319f6d4f44b30
Musqulu <galankonrad@gmail.com>
2022-09-26 21:36:24 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'daily-dev-tips': "#F89283"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
